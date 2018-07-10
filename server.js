389b2cbbdbcc022595c17e3461fa25d0f0f88b24
Nirav Patel <nrvpatel03@gmail.com>
2018-07-10 09:38:32 -0700

    Edit for heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var db = require("./models");
 
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -21,7 +21,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI =  process.env.MONGODB_URL || "mongodb://localhost/newsScraper";
+var MONGODB_URI =  process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
